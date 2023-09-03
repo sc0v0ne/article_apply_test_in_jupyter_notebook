fb9862defa58030bcc1443a508b8cab7e35436e6
sc0v1n0 <sc0v1n001@gmail.com>
2023-09-03 14:04:20 -0300

    test: divider two files tests

--- test/test_with_refact_replace_query_with_paramater.py
+++ test/test_with_refact_replace_query_with_paramater.py
@@ -1,19 +1,6 @@
 from testbook import testbook
 import pytest
 
-
-MESSAGE_EXCEPTION  = 'treatment_missing_values_original() takes 0 positional arguments but 1 was given'
-
-@testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
-def test_treatment_missing_values_original(tb):
-    function_notebook = tb.ref("treatment_missing_values_original")
-    group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']  
-    assert function_notebook() == None
-    with pytest.raises(Exception) as e_info:
-        function_notebook(group) == None
-    assert "takes 0 positional arguments but 1 was given" in str(e_info.value)
-
-
 @testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
 def test_treatment_missing_values_refc_01(tb):
     function_notebook = tb.ref("treatment_missing_values_refc_01")
