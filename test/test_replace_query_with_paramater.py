df48b3c0ea46538b70e678247f1257e5af7c9658
sc0v1n0 <sc0v1n001@gmail.com>
2023-09-03 13:13:23 -0300

    test: update tests, add Exception

--- test/test_replace_query_with_paramater.py
+++ test/test_replace_query_with_paramater.py
@@ -7,28 +7,21 @@ MESSAGE_EXCEPTION  = 'treatment_missing_values_original() takes 0 positional arg
 @testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
 def test_treatment_missing_values_original(tb):
     function_notebook = tb.ref("treatment_missing_values_original")
-    group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
-    
-    
+    group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']  
     assert function_notebook() == None
-    with pytest.raises(ValueError) as e_info:
-        raise ValueError(MESSAGE_EXCEPTION)
+    with pytest.raises(Exception) as e_info:
         function_notebook(group) == None
+    assert "takes 0 positional arguments but 1 was given" in str(e_info.value)
 
-    assert e_info.type is ValueError
-    assert e_info.value.args[0] == MESSAGE_EXCEPTION
 
 @testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
 def test_treatment_missing_values_refc_01(tb):
     function_notebook = tb.ref("treatment_missing_values_refc_01")
     group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
     assert function_notebook() == None
-    with pytest.raises(ValueError) as e_info:
-        raise ValueError(MESSAGE_EXCEPTION)
+    with pytest.raises(Exception) as e_info:
         function_notebook(group) == None
-
-    assert e_info.type is ValueError
-    assert e_info.value.args[0] == MESSAGE_EXCEPTION
+    assert "takes 0 positional arguments but 1 was given" in str(e_info.value)
     
 
 @testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
@@ -36,39 +29,27 @@ def test_XXXNEWtreatment_missing_values_refc_02(tb):
     function_notebook = tb.ref("XXXNEWtreatment_missing_values_refc_02")
     group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
     assert function_notebook(group) == None
-    with pytest.raises(ValueError) as e_info:
-        raise ValueError(MESSAGE_EXCEPTION)
+    with pytest.raises(Exception) as e_info:
         function_notebook() == None
-
-
-    assert e_info.type is ValueError
-    assert e_info.value.args[0] == MESSAGE_EXCEPTION
+    assert "missing 1 required positional argument" in str(e_info.value)
 
 @testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
 def test_XXXNEWtreatment_missing_values_refc_03_refactor(tb):
     function_notebook = tb.ref("XXXNEWtreatment_missing_values_refc_03_refactor")
     group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
     assert function_notebook() == None
-    with pytest.raises(ValueError) as e_info:
-        raise ValueError(MESSAGE_EXCEPTION)
+    with pytest.raises(Exception) as e_info:
         function_notebook(group) == None
-
-
-    assert e_info.type is ValueError
-    assert e_info.value.args[0] == MESSAGE_EXCEPTION
+    assert "takes 0 positional arguments but 1 was given" in str(e_info.value)
 
 @testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
 def test_XXXNEWtreatment_missing_values_refc_04(tb):
     function_notebook = tb.ref("XXXNEWtreatment_missing_values_refc_04")
     group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
     assert function_notebook(group) == None
-    with pytest.raises(ValueError) as e_info:
-        raise ValueError(MESSAGE_EXCEPTION)
+    with pytest.raises(Exception) as e_info:
         function_notebook() == None
-
-
-    assert e_info.type is ValueError
-    assert e_info.value.args[0] == MESSAGE_EXCEPTION
+    assert "missing 1 required positional argument" in str(e_info.value)
 
 
 @testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
@@ -76,12 +57,8 @@ def test_treatment_missing_values_refc_05(tb):
     function_notebook = tb.ref("treatment_missing_values_refc_05")
     group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
     assert function_notebook(group) == None
-    with pytest.raises(ValueError) as e_info:
-        raise ValueError(MESSAGE_EXCEPTION)
+    with pytest.raises(Exception) as e_info:
         function_notebook() == None
-
-
-    assert e_info.type is ValueError
-    assert e_info.value.args[0] == MESSAGE_EXCEPTION
+    assert "missing 1 required positional argument" in str(e_info.value)
 
 
