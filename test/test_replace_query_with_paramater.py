61791a4a0d51d5b8dd8c2a4785d223c3ea2ac1eb
sc0v1n0 <sc0v1n001@gmail.com>
2023-09-03 13:03:43 -0300

    test: update tests

--- test/test_replace_query_with_paramater.py
+++ test/test_replace_query_with_paramater.py
@@ -2,25 +2,86 @@ from testbook import testbook
 import pytest
 
 
+MESSAGE_EXCEPTION  = 'treatment_missing_values_original() takes 0 positional arguments but 1 was given'
+
 @testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
 def test_treatment_missing_values_original(tb):
     function_notebook = tb.ref("treatment_missing_values_original")
     group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
-    message_exception  = 'treatment_missing_values_original() takes 0 positional arguments but 1 was given'
+    
     
     assert function_notebook() == None
     with pytest.raises(ValueError) as e_info:
-        raise ValueError(message_exception)
+        raise ValueError(MESSAGE_EXCEPTION)
         function_notebook(group) == None
 
-        assert e_info.type is ValueError
-        assert e_info.value.args[0] == message_exception
+    assert e_info.type is ValueError
+    assert e_info.value.args[0] == MESSAGE_EXCEPTION
+
+@testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
+def test_treatment_missing_values_refc_01(tb):
+    function_notebook = tb.ref("treatment_missing_values_refc_01")
+    group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
+    assert function_notebook() == None
+    with pytest.raises(ValueError) as e_info:
+        raise ValueError(MESSAGE_EXCEPTION)
+        function_notebook(group) == None
+
+    assert e_info.type is ValueError
+    assert e_info.value.args[0] == MESSAGE_EXCEPTION
     
+
+@testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
+def test_XXXNEWtreatment_missing_values_refc_02(tb):
+    function_notebook = tb.ref("XXXNEWtreatment_missing_values_refc_02")
+    group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
+    assert function_notebook(group) == None
+    with pytest.raises(ValueError) as e_info:
+        raise ValueError(MESSAGE_EXCEPTION)
+        function_notebook() == None
+
+
+    assert e_info.type is ValueError
+    assert e_info.value.args[0] == MESSAGE_EXCEPTION
+
+@testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
+def test_XXXNEWtreatment_missing_values_refc_03_refactor(tb):
+    function_notebook = tb.ref("XXXNEWtreatment_missing_values_refc_03_refactor")
+    group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
+    assert function_notebook() == None
+    with pytest.raises(ValueError) as e_info:
+        raise ValueError(MESSAGE_EXCEPTION)
+        function_notebook(group) == None
+
+
+    assert e_info.type is ValueError
+    assert e_info.value.args[0] == MESSAGE_EXCEPTION
+
+@testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
+def test_XXXNEWtreatment_missing_values_refc_04(tb):
+    function_notebook = tb.ref("XXXNEWtreatment_missing_values_refc_04")
+    group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
+    assert function_notebook(group) == None
+    with pytest.raises(ValueError) as e_info:
+        raise ValueError(MESSAGE_EXCEPTION)
+        function_notebook() == None
+
+
+    assert e_info.type is ValueError
+    assert e_info.value.args[0] == MESSAGE_EXCEPTION
+
 
 @testbook('./notebook_replace_query_with_parameter.ipynb', execute=True)
 def test_treatment_missing_values_refc_05(tb):
     function_notebook = tb.ref("treatment_missing_values_refc_05")
     group = ['BI_RADS', 'age', 'shape', 'margin', 'density', 'severity']
     assert function_notebook(group) == None
+    with pytest.raises(ValueError) as e_info:
+        raise ValueError(MESSAGE_EXCEPTION)
+        function_notebook() == None
+
+
+    assert e_info.type is ValueError
+    assert e_info.value.args[0] == MESSAGE_EXCEPTION
 
 
